9e449bf49e5aee9b9c9305ae0e1a7a7a33dafc08
GladysKariuki <Gladysnjeri56@gmail.com>
2025-02-18 05:32:51 +0300

    display current path and api response data

--- leanbit-file-engine-ui/src/App.jsx
+++ leanbit-file-engine-ui/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/themes/dark.css';
+import SlCard from '@shoelace-style/shoelace/dist/components/card/card.js';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.0/cdn/');
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPath, setCurrentPath] = useState('/');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3030/api/files/?path=${currentPath}`, {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        contentType: 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    }).then(result => {
+      console.log(result);
+      setData(result);
+    }).catch(error => {
+      console.error(error);
+    })
+  }, [currentPath]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <div className='title-section'>
+          <h1>LeanBit File Engine</h1>
+          <p>Current Path: {currentPath}</p>
+        </div>
+        <div className='subtitle-section'>
+          <h4>Files &amp; Folders</h4>
+        </div>
+        <div className='files-and-folders-section'>
+          {data.length === 0 ? (
+            <p>No files or folders</p>
+          ) : (data.map((apiData, index) => (
+            <sl-card key={index}>
+              <p>{apiData.name}</p>
+            </sl-card>
+          )))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
