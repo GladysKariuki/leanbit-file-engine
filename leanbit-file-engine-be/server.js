0e5de7b673f7c393bbe8e9669430e88f620b0796
GladysKariuki <Gladysnjeri56@gmail.com>
2025-02-18 05:32:12 +0300

    add cors

--- leanbit-file-engine-be/server.js
+++ leanbit-file-engine-be/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import FileManager from './FileManager.js';
 
 const app = express();
 const port = 3030;
 
 app.use(express.json());
+app.use(cors());
 
 const fileManager = new FileManager();
 
